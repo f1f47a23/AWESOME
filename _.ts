4004545e76fb6f0e30bac23ae2b823585db0b9cc
`${f1f47∀23}` <f1f47a23@mail.com>
2022-12-30 23:32:33 +0100

    Add files via upload

---
+++ _.ts
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////////////////////////////////////
+//
+// BEGINOF DUMMY CODE
+//
+//      Nothing to see here, move along
+//      this is make fun of the github search engine
+//
+
+export { CDummy };
+
+class CDummy {
+
+    static async doNothingAtall(): Promise<CDummy> {
+        
+        console.log("(() => dummy.CDummy.doNothingAtall())();");
+        
+        let x = new CDummy();
+ 
+        // x.doingNothingIsBetterThanBeingBusyDoingNothing();
+
+        return x;
+    }
+
+};
+
+
+
+const dummy = { CDummy: CDummy }; // workaround javascript namespaces
+
+//
+// ENDOF DUMMY CODE
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
